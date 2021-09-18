495610a6ccdf03a0721424544a471a0673423ed5
al-abbas-nz <al.mus930@gmail.com>
2021-09-19 01:39:05 +1200

    add first NFT solidity contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    //tokenIds will give a unique ID for minted tokens. Counters will allow us to continually increment the IDs.
+    Counters.Counter private _tokenIds;
+
+    //the address of the marketplace that we want to allow the NFT to be able to interact with, and vise versa. (transact/change ownership from seperate contract.)
+    address contractAddress;
+
+    constructor(address marketplaceAddres) ERC721("Metaverse Tokens", "METT"){
+        contractAddress = marketplaceAddress
+    };
+
+    //the function for minting tokens.
+    function createToken(string memory tokenURI) public returns (uint) {
+        
+        //increment the tokenIds: increment the values from 0
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        //allow transactions between users within another contract.
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+    };
+
+}
+
+
---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+const fs = require('fs');
+
+//metamask wallet private key
+const privateKey = fs.readFileSync('.secret').toString();
+
+const projectId = process.env.INFURA_PROJECT_ID;
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+  //configure the different networks
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+  },
+  solidity: '0.8.4',
+};
