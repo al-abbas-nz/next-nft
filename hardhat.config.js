55cbae4fe0dfd0e5603fae91dbed6ba498f87a3a
al-abbas-nz <al.mus930@gmail.com>
2021-09-20 14:21:27 +1200

    attempt deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,12 +17,12 @@ module.exports = {
       chainId: 1337,
     },
     mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${projectId.toString()}`,
-      accounts: [privateKey.toString()],
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
     },
     mainnet: {
-      url: `https://polygon-mainnet.infura.io/v3/${projectId.toString()}`,
-      accounts: [privateKey.toString()],
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
     },
   },
   solidity: '0.8.4',
